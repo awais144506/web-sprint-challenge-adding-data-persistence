06cec4c018067244e481538c6afc91768f8bbee7
awais144506 <awaisrasheed68@gmail.com>
2024-11-28 23:30:47 +0500

    Good

--- api/task/model.js
+++ api/task/model.js
@@ -1,2 +1,14 @@
 //import DB
 const db = require('../../data/dbConfig')
+//Get Tasks Functions
+async function getTasks() {
+    return await db('tasks')
+}
+//Check the Task ID
+
+//Post Task
+
+
+module.exports = {
+    getTasks
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,12 +1,19 @@
 //Import Files
 const express = require('express')
+const Task = require('./model')
 //Invoke Router
 const router = express.Router()
 
+//Get Tasks
+router.get('/', (req, res, next) => {
+    Task.getTasks()
+    .then(tk=>{
+        res.status(200).json(tk)
+    })
+    .catch(next)
+})
 
-
-
-
+//Post Tasks
 //Next Function 
 router.use((error, req, res, next) => {
     res.status(error.status || 500).json({
