c5dded8e15a40916453b22b290a83a8fc2bee974
awais144506 <awaisrasheed68@gmail.com>
2024-11-29 14:14:53 +0500

    Done

--- api/task/model.js
+++ api/task/model.js
@@ -2,13 +2,43 @@
 const db = require('../../data/dbConfig')
 //Get Tasks Functions
 async function getTasks() {
-    return await db('tasks')
+    /*
+select 
+t.task_id,t.task_description,t.task_notes,t.task_completed,p.project_name,p.project_description
+from tasks as t
+join projects as p
+on t.project_id=p.project_id
+    */
+    const rows = await db('tasks as t')
+        .select('t.task_id',
+            't.task_description',
+            't.task_notes',
+            't.task_completed',
+            'p.project_name',
+            'p.project_description')
+            .join('projects as p','t.project_id','p.project_id')
+    const result = rows.map(row => ({
+        ...row,
+        task_completed: Boolean(row.task_completed)
+    }))
+    return result
 }
-//Check the Task ID
-
+//Return the new Task 
+async function newTask(id) {
+    const task = await db('tasks').where('task_id',id).first()
+    const finalTask = {...task,task_completed:Boolean(task.task_completed)}
+    return finalTask
+}
 //Post Task
-
+async function postNewTask(task) {
+    return await db('tasks')
+    .insert(task)
+    .then(([task_id])=>{
+        return newTask(task_id)
+    })
+}
 
 module.exports = {
-    getTasks
+    getTasks,
+    postNewTask
 }
--- api/task/router.js
+++ api/task/router.js
@@ -14,6 +14,14 @@ router.get('/', (req, res, next) => {
 })
 
 //Post Tasks
+router.post('/',(req,res,next)=>{
+    Task.postNewTask(req.body)
+    .then(tk=>{
+        res.status(201).json(tk)
+    })
+     .catch(next)
+})
+
 //Next Function 
 router.use((error, req, res, next) => {
     res.status(error.status || 500).json({
